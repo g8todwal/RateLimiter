6467a5da511acdb073bf88a8de243bdd26295d07
Todwal <gaurav.todwal@xecurify.com>
2026-05-27 14:58:44 +0530

    Bolier Plate

---
+++ rateify/src/main/java/com/todwal/rateify/RateifyApplication.java
@@ -0,0 +1,13 @@
+package com.todwal.rateify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateifyApplication.class, args);
+	}
+
+}
